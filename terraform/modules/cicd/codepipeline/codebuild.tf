5feebff88ef259ec404136962034284b1ec65551
AbMoiz1 <abdulmoiztahir21@gmail.com>
2026-04-29 22:05:33 +0500

    fix: upgrade CodeBuild to medium instance, fix buildspec zip paths

--- terraform/modules/cicd/codepipeline/codebuild.tf
+++ terraform/modules/cicd/codepipeline/codebuild.tf
@@ -10,7 +10,7 @@ resource "aws_codebuild_project" "node_services" {
   artifacts { type = "CODEPIPELINE" }
 
   environment {
-    compute_type    = "BUILD_GENERAL1_SMALL"
+    compute_type    = "BUILD_GENERAL1_MEDIUM"
     image           = "aws/codebuild/amazonlinux2-x86_64-standard:5.0"
     type            = "LINUX_CONTAINER"
     privileged_mode = false
